881ed32543c1491dde634e4426002c611e6dc70b
Nicklas Winther <nick3845@edu.campusvejle.dk>
2020-01-21 14:40:04 +0100

    Add project files.

---
+++ WpfAppIntroduktionString/BIZ/ClassBIZ.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using WpfAppIntroduktionString.IO;
+
+namespace WpfAppIntroduktionString.BIZ
+{
+    class ClassBIZ
+    {
+        public ClassBIZ()
+        {
+
+        }
+        ClassIO classIO = new ClassIO();
+
+        public void GetText(TextBox textBox)
+        {
+            classIO.GetTestString(textBox);
+        }
+
+        //Opgave 1
+        public int CountAllLines(TextBox textBox)
+        {
+            int counter = textBox.LineCount;
+            for (int i = 0; i < counter; i++)
+            {
+                if (textBox.GetLineText(i).Trim().Length == 0)
+                {
+                    counter--;
+                }
+            }
+            return counter;
+        }
+
+        //Opgave 2
+        public int CountAllChars(TextBox textBox)
+        {
+            return textBox.Text.Length;
+        }
+    }
+}
---
+++ WpfAppIntroduktionString/IO/ClassIO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfAppIntroduktionString.IO
+{
+    class ClassIO
+    {
+        public ClassIO()
+        {
+
+        }
+        //Opgave 1
+        public void GetTestString(TextBox textBox)
+        {
+            textBox.Text = File.ReadAllText("../../Admiralen.txt");
+        }
+
+    }
+}
